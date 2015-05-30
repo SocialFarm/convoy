befe4c5a2d6c99c8cb1a02cb702469325abe7e78
Prerak <jainprerak@gmail.com>
2015-05-30 19:44:41 +0530

    refactoring

--- js/srglobal.js
+++ js/srglobal.js
@@ -1,14 +1,12 @@
 // A common place holder for all the global variables and constants.
 
 var SR = {};
-SR.couchDB = {
-    URL : 'http://socialfarm.org/couchdb/social_ride/',
-    viewUrl : this.URL + '_design/info/_view/',
-    nearbyOfferView : this.viewUrl + 'nearby_offer',
-    nearbyReqView : this.viewUrl + 'nearby_request',
-    myOfferView : this.viewUrl + 'my_offer',
-    myReqView : this.viewUrl + 'my_request'
-};
+SR.couchDB.URL = 'http://socialfarm.org/couchdb/social_ride/';
+SR.couchDB.viewUrl = SR.couchDB.URL + '_design/info/_view/';
+SR.couchDB.nearbyOfferView = SR.couchDB.URL + 'nearby_offer';
+SR.couchDB.nearbyReqView = SR.couchDB.URL + 'nearby_request';
+SR.couchDB.myOfferView = SR.couchDB.URL + 'my_offer';
+SR.couchDB.myReqView = SR.couchDB.URL + 'my_request';
 
 SR.divId = {
     ride: {
@@ -18,4 +16,5 @@ SR.divId = {
     user: {
         request: 'tab_my_req',
         offer: 'tab_my_offer'
+    }
 };
